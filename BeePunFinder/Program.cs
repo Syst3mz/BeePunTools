c6be32ce88aa572d6b4142f09246c5b83c214b0c
Syst3mz <ethanansburgstda@gmail.com>
2023-03-02 00:51:17 -0700

    Made the system output all synonyms.

--- BeePunFinder/Program.cs
+++ BeePunFinder/Program.cs
@@ -11,7 +11,7 @@ namespace BeePunFinder
     {
         static void Main(string[] args)
         {
-            var nm = MessageConvert("Fourscore and seven years ago our fathers brought forth, on this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal. Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived, and so dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting-place for those who here gave their lives, that that nation might live. It is altogether fitting and proper that we should do this. But, in a larger sense, we cannot dedicate, we cannot consecrate—we cannot hallow—this ground. The brave men, living and dead, who struggled here, have consecrated it far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us—that from these honored dead we take increased devotion to that cause for which they here gave the last full measure of devotion—that we here highly resolve that these dead shall not have died in vain—that this nation, under God, shall have a new birth of freedom, and that government of the people, by the people, for the people, shall not perish from the earth.");
+            var nm = MessageConvert("all of the things in the [()] can be made into bee puns and are synonyms to the original word in the message");
             Console.WriteLine(nm);
         }
 
@@ -87,24 +87,34 @@ namespace BeePunFinder
 
         private static string FindReplacement(string clean, Dictionary<string, TEntry> lookup, HashSet<string> candidates)
         {
-            if (lookup.ContainsKey(clean.ToLower()))
+            if (!lookup.ContainsKey(clean.ToLower()))
             {
-                var entry = lookup[clean.ToLower()];
-                if (entry.synonyms.Count >= 1)
-                {
-                    foreach (string synonym in entry.synonyms)
-                    {
-                        if (candidates.Contains(synonym))
-                        {
-                            return $"[({synonym})]";
-                        }
-                    }
-
-                    return clean;
-                }
                 return clean;
             }
+
+            var entry = lookup[clean.ToLower()];
+            if (entry.synonyms.Count < 1)
+            {
+                return clean;
+            }
+            
+            List<string> validSwaps = new List<string>();
+            foreach (string synonym in entry.synonyms)
+            {
+                if (candidates.Contains(synonym))
+                {
+                    validSwaps.Add(synonym);
+                }
+            }
+
+            if (validSwaps.Count >= 1)
+            {
+                var o = string.Join(",", validSwaps);
+                return $"[({o})]";
+            }
+
             return clean;
+
         }
     }
 
